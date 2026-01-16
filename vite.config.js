47fe0c820e2dad3d238d73bc25a94f9314512b13
3bitdiy <stevan@3bit.diy>
2026-01-16 17:18:23 +0100

    Update Vite config for multiple HTML entry points
    Added Rollup input options to support both index.html and arhiv-u-slikama.html as entry points. Also fixed the base path to include a trailing slash.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/sokolski-dom",
+  base: "/sokolski-dom/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        arhiv: resolve(__dirname, "arhiv-u-slikama.html"),
+      },
+    },
+  },
 });
